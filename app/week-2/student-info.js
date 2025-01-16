37f29a183f5c616e034599ab73ab058dea22d046
mitmakadia2006 <mitmakadia2006@gmail.com>
2025-01-16 12:57:03 -0700

    Completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+export default function StudentInfo(){
+return(
+    <main>
+        <h1>Name: Mit Makadia</h1>
+        <p>
+            My git account link:
+            <Link href="https://github.com/Mit459"> Click me </Link>
+        </p>
+    </main>
+);
+}
